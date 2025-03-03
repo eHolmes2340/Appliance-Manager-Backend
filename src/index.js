a9da9756cf25cf66d4cafcdd32f708f55386e7d5
Erik Holmes <erikholmes101@gmail.com>
2025-03-03 18:02:16 -0500

    trying google cloud

--- src/index.js
+++ src/index.js
@@ -21,8 +21,8 @@ app.use('/api', userRoutes);
 app.use('/api', applianceRoutes);
 app.use('/api',health);
 
-const server = app.listen(PORT, '10.0.0.105', () => {
-  console.log(`Server running on http://10.0.0.105:${PORT}`);
+const server = app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
 
 const shutdown = () => {
